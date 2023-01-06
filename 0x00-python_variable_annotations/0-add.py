dd44ce7568ccb9b88681a01710c843080bb7f938
Veralee225 <Veralee225@gmail.com>
2023-01-06 01:03:36 +0100

    feat: task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
-def greeting(name, str) -> str:
-    return 'Hello ' + name
-    
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''Adds two floating-point numbers.
+    '''
+    return a + b
